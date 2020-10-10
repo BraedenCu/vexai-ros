4067996d87e1b0106d4da90b0f02eb16413391d6
BraedenCu <braedencullen120@gmail.com>
2020-10-10 15:46:17 -0500

    t

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,6 +5,10 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("mybot_ws/src/mybot_control")
+subdirs("mybot_ws/src/mybot_description")
+subdirs("mybot_ws/src/mybot_gazebo")
+subdirs("mybot_ws/src/mybot_navigation")
 subdirs("realsense/realsense-ros/realsense2_description")
 subdirs("realsense/realsense-ros/realsense2_camera")
 subdirs("gazeboSimulation")
